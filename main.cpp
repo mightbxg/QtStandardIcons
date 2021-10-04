5e810132b69daf6f6b0ed9d855e7fa71c8d86287
mightbxg <mightbxg@hotmail.com>
2021-10-04 20:59:24 +0800

    v1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "widget.h"
 
 #include <QApplication>
+#include <iostream>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
+    std::cout << "note: use -style to set icon style." << std::endl;
+
     QApplication a(argc, argv);
     Widget w;
     w.show();
--- widget.cpp
+++ widget.cpp
@@ -1,11 +1,30 @@
 #include "widget.h"
+#include <QLayout>
+#include <QMetaEnum>
+#include <QPushButton>
+#include <QStyle>
+#include <iostream>
 
-Widget::Widget(QWidget *parent)
+Widget::Widget(QWidget* parent)
     : QWidget(parent)
 {
+    auto layout = new QGridLayout;
+    setLayout(layout);
+
+    constexpr int cols = 6;
+    const auto meta_enum = QMetaEnum::fromType<QStyle::StandardPixmap>();
+    for (int i = 0; i < 78; ++i) {
+        auto btn = new QPushButton(tr("%1").arg(i, 2));
+        auto icon = this->style()->standardIcon(QStyle::StandardPixmap(i));
+        btn->setIcon(icon);
+        QString name = meta_enum.valueToKey(i);
+        connect(btn, &QPushButton::clicked, [name, i]() {
+            std::cout << i << ": " << name.toStdString() << std::endl;
+        });
+        layout->addWidget(btn, i / cols, i % cols);
+    }
 }
 
 Widget::~Widget()
 {
 }
-
