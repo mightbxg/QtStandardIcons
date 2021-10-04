b199293c7c3a7bb51b4df0eec5f905957214e59c
mightbxg <mightbxg@hotmail.com>
2021-10-04 21:30:53 +0800

    add tooltips

--- widget.cpp
+++ widget.cpp
@@ -14,10 +14,11 @@ Widget::Widget(QWidget* parent)
     constexpr int cols = 6;
     const auto meta_enum = QMetaEnum::fromType<QStyle::StandardPixmap>();
     for (int i = 0; i < 78; ++i) {
+        QString name = meta_enum.valueToKey(i);
         auto btn = new QPushButton(tr("%1").arg(i, 2));
         auto icon = this->style()->standardIcon(QStyle::StandardPixmap(i));
         btn->setIcon(icon);
-        QString name = meta_enum.valueToKey(i);
+        btn->setToolTip(name);
         connect(btn, &QPushButton::clicked, [name, i]() {
             std::cout << i << ": " << name.toStdString() << std::endl;
         });
